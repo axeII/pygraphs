b4c29db4525591e59d3624ed740aa9df31c0d257
Ales Lerch <ales.lerch@gmail.com>
2017-03-12 00:37:16 +0100

    new_task improved, makefile fixed, test in added

--- src/new_task.sh
+++ src/new_task.sh
@@ -3,14 +3,19 @@
 INFO="Paste info about this task here, remeber to write what code is about."
 AUTHOR="__author__ = \'Ales Lerch\'"
 
-TEXT="#import os
+TEXT="import os
 import sys\n
-Importing specific library\n
+#Importing specific library\n
 if os.path.isdir('./lib'):
     sys.path.append('./lib')
 else:
     sys.path.append('../lib')\n
 from graph_lib.pygraphs import *\n\n"
+
+FUNCTIONS="def get_input_data():\n\tpass
+def parse_data(data):\n\tpass\n
+def solve_with_graph():\n\tpass\n
+def print_final():\n\tpass\n"
 
 for file in $@ ; do
 	NAME="$file.py" ;
@@ -19,5 +24,6 @@ for file in $@ ; do
 	printf "$INFO\n\"\"\"\n\n" >> $NAME ;
 	printf "$AUTHOR\n\n" >> $NAME ;
 	printf "$TEXT" >> $NAME ;
-	printf "Generated $NAME file"
+	printf "$FUNCTIONS" >> $NAME ;
+	printf "Generated $NAME file\n"
 done
